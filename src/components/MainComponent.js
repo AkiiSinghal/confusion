cdde7a6e366aa0c625e83e1cdaa443ec14a15d5b
Akshat Singhal <akshatsinghal333@gmail.com>
2020-07-25 23:41:21 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,15 +6,20 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -25,7 +30,11 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-          <Home />
+        <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
     
@@ -33,16 +42,17 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
-            <Route path='/home' component={HomePage} />
-            <Route exact path='/menu' component={() => {
-                return (
-                    <>
-                        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                    </>
-                );
-            }} />
-            <Redirect to="/home" />
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => {
+            return (
+                <>
+                    <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                    <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                </>
+            );
+          }} />
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </div>
